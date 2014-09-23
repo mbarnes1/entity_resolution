fedb1678ab0305e143d3226b55a396a405ce3d76
Matt Barnes <barnes.matt.j@gmail.com>
2014-09-23 15:47:23 -0400

    Generalized record class tested. Removed unnecessary test files in favor of one hand-crafted example (test_annotations.csv) and one subsample of 10,000 real ads (test_annotations_10000.csv)

--- record.py
+++ record.py
@@ -1,305 +1,75 @@
 # This is the main utilities file for Traffic Jam
 import datetime
+from itertools import izip  # Uses iterator instead of list (less memory)
 
 
 # A single record, i.e. ad or merge of ads
 class Record(object):
-    def __init__(self):
-        self.ads = set()  # a set of all this person's ads
-
-        self.posters = set()
-        self.sites = set()
-        self.dates = set()  # post time, using datetime object
-        self.states = set()
-        self.cities = set()
-        self.perspective_1st = 0  # cannot satisfy ICAR
-        self.perspective_3rd = 0  # cannot satisfy ICAR
-        self.names = set()
-        self.ages = set()
-        self.costs = set()
-        self.heights = set()
-        self.weights = set()
-        self.cups = set()
-        self.chests = set()
-        self.waists = set()
-        self.hips = set()
-        self.ethnicities = set()
-        self.skincolors = set()
-        self.eyecolors = set()
-        self.haircolors = set()
-        self.phones = set()
-        self.emails = set()
-        self.urls = set()
-        self.medias = set()
-        self.images = set()
-
-        #self.restrictiontype = UNUSED
-        #self.restrictionage = UNUSED
-        #self.restrictionethnicity = UNUSED
+    """
+    This object represents a single ad or entity.
+    """
+    def __init__(self, ad_number, number_features):
+        self.features = list()
+        for _ in range(0, number_features):
+            self.features.append(set())
+        self.ads = {ad_number}
 
     def merge(self, r2):
-        self.phones.update(r2.phones)
-        self.posters.update(r2.posters)
-        self.ads.update(r2.ads)
-        self.sites.update(r2.sites)
-        self.dates.update(r2.dates)
-        self.states.update(r2.states)
-        self.cities.update(r2.cities)
-        self.names.update(r2.names)
-        self.ages.update(r2.ages)
-        self.costs.update(r2.costs)
-        self.heights.update(r2.heights)
-        self.weights.update(r2.weights)
-        self.cups.update(r2.cups)
-        self.chests.update(r2.chests)
-        self.waists.update(r2.waists)
-        self.hips.update(r2.hips)
-        self.ethnicities.update(r2.ethnicities)
-        self.skincolors.update(r2.skincolors)
-        self.eyecolors.update(r2.eyecolors)
-        self.haircolors.update(r2.haircolors)
-        self.emails.update(r2.emails)
-        self.urls.update(r2.urls)
-        self.medias.update(r2.medias)
-        self.images.update(r2.images)
-        self.add_perspective_1st(r2.perspective_1st)
-        self.add_perspective_3rd(r2.perspective_3rd)
+        """
+        Merges current record in-place with record r2
+        :param r2: Record to merge into self
+        """
+        for feature1, feature2 in izip(self.features, r2.features):
+            feature1.update(feature2)
 
-    def initialize_from_annotation(self, annotation, ad_number):
-        features = annotation.split(',')  # extract features
-        posterids = features[0].split(';')  # STRONG
-        sites = features[1].split(';')  # weak
-        dates = features[2].split(';')  # weak
-        states = features[3].split(';')  # weak
-        cities = features[4].split(';')  # weak
-        # Not using 1st and 3rd perspective. Does not add up correctly with multiple merges. Would neeed to calculate
-        # by referencing original ads
-        perspectives_1st = features[5].split(';')  # actual count of 1st and 3rd perspectives could catch duplicates. 1st v 3rd winner
-        perspectives_3rd = features[6].split(';')  # is a weak feature
-        names = features[7].split(';')  # weak
-        ages = features[8].split(';')  # weak
-        costs = features[9].split(';')  # weak
-        heights_ft = features[10].split(';')  # weak
-        heights_in = features[11].split(';')  # weak
-        weights = features[12].split(';')  # weak
-        cups = features[13].split(';')  # weak
-        chests = features[14].split(';')  # weak
-        waists = features[15].split(';')  # weak
-        hips = features[16].split(';')  # weak
-        ethnicities = features[17].split(';')  # weak
-        skincolors = features[18].split(';')  # weak
-        eyecolors = features[19].split(';')  # weak
-        haircolors = features[20].split(';')  # weak
-        restrictions_type = features[21].split(';')  # what is this?
-        restrictions_ethnicity = features[22].split(';')  # what is this?
-        restrictions_age = features[23].split(';')  # what is this?
-        phones = features[24].split(';')  # STRONG. extract multiple phone numbers
-        areacodes_state = features[25].split(';')  # STRONG
-        areacodes_cities = features[26].split(';')  # STRONG
-        emails = features[27].split(';')  # STRONG
-        urls = features[28].split(';')  # STRONG?
-        medias = features[29].split(';')  # STRONG?
-        images = features[30].split(';')  # STRONG?
+    def initialize_from_annotation(self, features, feature_types):
+        """
+        Populates record fields from a single line in a csv file, where commas are used to separate different types of
+        features and semicolons (;) are used to separate multiple of the same type of feature (e.g. multiple phone
+        numbers)
+        :param features: list of features
+        :param feature_types: Variable type
+        """
+        if len(self.features) != len(features):
+            raise Exception('Feature dimension mismatch')
+        if len(self.features) != len(feature_types):
+            raise Exception('Feature dimension mismatch')
+        index = 0
+        for feature, feature_type in izip(features, feature_types):
+            sub_features = feature.split(';')
+            for f in sub_features:
+                if (f != '') & (f != 'none'):
+                    if feature_type == 'string':
+                        self.features[index].add(f)
+                    elif feature_type == 'int':
+                        self.features[index].add(int(f))
+                    elif feature_type == 'date':
+                        self.features[index].add(get_date(f))
+            index += 1
 
-        self.add_ad(ad_number)
-
-        for posterid in posterids:
-            if (posterid != '') & (posterid != 'none'):
-                self.add_poster(posterid)
-        for site in sites:
-            if (site != '') & (site != 'none'):
-                self.add_site(site)
-        for date in dates:
-            if (date != '') & (date != 'none'):
-                self.add_date(date)
-        for state in states:
-            if (state != '') & (state != 'none'):
-                self.add_state(state)
-        for city in cities:
-            if (city != '') & (city != 'none'):
-                self.add_city(city)
-        for perspective_1st in perspectives_1st:
-            if (perspective_1st != '') & (perspective_1st != 'none'):
-                self.add_perspective_1st(int(perspective_1st))
-        for perspective_3rd in perspectives_3rd:
-            if (perspective_3rd != '') & (perspective_3rd != 'none'):
-                self.add_perspective_3rd(int(perspective_3rd))
-        for name in names:
-            if (name != '') & (name != 'none'):
-                self.add_name(name)
-        for age in ages:
-            if (age != '') & (age != 'none'):
-                self.add_age(int(age))
-        for cost in costs:
-            if (cost != '') & (cost != 'none'):
-                self.add_cost(cost)
-        for height_ft, height_in in zip(heights_ft, heights_in):
-            if (height_ft != '') & (height_ft != 'none'):
-                if (height_in == '') | (height_in == 'none'):
-                    height_in = 0
-                self.add_height(int(float(height_ft))*12 + int(float(height_in)))
-        for weight in weights:
-            if (weight != '') & (weight != 'none'):
-                self.add_weight(int(float(weight)))
-        for cup in cups:
-            if (cup != '') & (cup != 'none'):
-                self.add_cup(cup)
-        for chest in chests:
-            if (chest != '') & (chest != 'none'):
-                self.add_chest(int(float(chest)))
-        for waist in waists:
-            if (waist != '') & (waist != 'none'):
-                self.add_waist(int(float(waist)))
-        for hip in hips:
-            if (hip != '') & (hip != 'none'):
-                self.add_hip(int(float(hip)))
-        for ethnicity in ethnicities:
-            if (ethnicity != '') & (ethnicity != 'none'):
-                self.add_ethnicity(ethnicity)
-        for skincolor in skincolors:
-            if (skincolor != '') & (skincolor != 'none'):
-                self.add_skincolor(skincolor)
-        for eyecolor in eyecolors:
-            if (eyecolor != '') & (eyecolor != 'none'):
-                self.add_eyecolor(eyecolor)
-        for haircolor in haircolors:
-            if (haircolor != '') & (haircolor != 'none'):
-                self.add_haircolor(haircolor)
-        for phone in phones:
-            if (phone != '') & (phone != 'none'):
-                self.add_phone(phone)
-        for email in emails:
-            if (email != '') & (email != 'none'):
-                self.add_email(email)
-        for url in urls:
-            if (url != '') & (url != 'none'):
-                self.add_url(url)
-        for media in medias:
-            if (media != '') & (media != 'none'):
-                self.add_media(media)
-        for image in images:
-            if (image != '') & (image != 'none'):
-                self.add_image(image)
-
-    def display(self):
-        print 'Ads: ', self.ads
-        print 'Phones: ', self.phones
-        print 'Names: ', self.names
-        print 'Poster IDs: ', self.posters
-        print 'Emails: ', self.emails
-        print 'Dates: ', self.dates
-        print 'Sites: ', self.sites
-        print 'States: ', self.states
-        print 'Cities: ', self.cities
-        print 'Ages: ', self.ages
-        print 'Costs: ', self.costs
-        print 'Heights: ', self.heights
-        print 'Weights: ', self.weights
-        print 'Cup sizes: ', self.cups
-        print 'Chest sizes: ', self.chests
-        print 'Waist sizes: ', self.waists
-        print 'Hip sizes: ', self.hips
-        print 'Ethnicities: ', self.ethnicities
-        print 'Skin colors: ', self.skincolors
-        print 'Eye colors: ', self.eyecolors
-        print 'Hair colors: ', self.haircolors
-        print '1st person references: ', self.perspective_1st
-        print '3rd person references: ', self.perspective_3rd
-        print 'Images: ', self.images
-        print 'URLS: ', self.urls
-        print 'Media: ', self.medias
-
-    def add_ad(self, ad):
-        self.ads.add(ad)
-
-    def add_phone(self, phone):
-        self.phones.add(phone)
-
-    def add_poster(self, poster):
-        self.posters.add(poster)
-
-    def add_site(self, site):
-        self.sites.add(site)
-
-    def add_date(self, date):
-        try:
-            mo = month(date[4:7])  # month
-            d = int(date[8:10])  # day
-            h = int(date[11:13])  # hour
-            mi = int(date[14:16])  # min
-            s = int(date[17:19])  # sec
-            z = date[20:23]  # time zone. Assuming EST for now
-            y = int(date[24:29])  # year
-            time = datetime.datetime(y, mo, d, h, mi, s)
-            self.dates.add(time)
-        except:
-            pass
-
-    def add_state(self, state):
-        self.states.add(state)
-
-    def add_city(self, city):
-        self.cities.add(city)
-
-    def add_name(self, name):
-        self.names.add(name)
-
-    def add_age(self, age):
-        self.ages.add(age)
-
-    def add_cost(self, cost):
-        self.costs.add(cost)
-
-    def add_height(self, height_in_inches):
-        self.heights.add(height_in_inches)
-
-    def add_weight(self, weight):
-        self.weights.add(weight)
-
-    def add_cup(self, cup):
-        self.cups.add(cup)
-
-    def add_chest(self, chest):
-        self.chests.add(chest)
-
-    def add_waist(self, waist):
-        self.waists.add(waist)
-
-    def add_hip(self, hip):
-        self.hips.add(hip)
-
-    def add_ethnicity(self, ethnicity):
-        self.ethnicities.add(ethnicity)
-
-    def add_skincolor(self, skincolor):
-        self.skincolors.add(skincolor)
-
-    def add_eyecolor(self, eyecolor):
-        self.eyecolors.add(eyecolor)
-
-    def add_haircolor(self, haircolor):
-        self.haircolors.add(haircolor)
-
-    def add_email(self, email):
-        self.emails.add(email)
-
-    def add_url(self, url):
-        self.urls.add(url)
-
-    def add_media(self, media):
-        self.medias.add(media)
-
-    def add_image(self, image):
-        self.images.add(image)
-
-    def add_perspective_1st(self, count):
-        self.perspective_1st = 0  # += count  # not using, see comment above
-
-    def add_perspective_3rd(self, count):
-        self.perspective_3rd = 0  # += count
+    def display(self, names):
+        for name, feature in izip(names, self.features):
+            print '{0}: {1}'.format(name, feature)
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
+
+def get_date(date):
+    """
+    Gets datetime object from string annotation
+    :param date: String in format 'Thu Jan 30 02:41:11 EST 2014'
+    :return time: Datetime object, assuming EST
+    """
+    mo = month(date[4:7])  # month
+    d = int(date[8:10])  # day
+    h = int(date[11:13])  # hour
+    mi = int(date[14:16])  # min
+    s = int(date[17:19])  # sec
+    z = date[20:23]  # time zone. Assuming EST for now
+    y = int(date[24:29])  # year
+    time = datetime.datetime(y, mo, d, h, mi, s)
+    return time
 
 
 def month(x):
--- test/test_record.py
+++ test/test_record.py
@@ -1,87 +1,81 @@
 import unittest
 import datetime
 import sys
-
 sys.path.append('../../')
-from entity_resolution.utils import Record, month
-
+from entity_resolution.record import Record, month, get_date
 __author__ = 'mbarnes1'
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self._r1 = Record()
-        self._r2 = Record()
-        self._annotation_full = '9552601,neworleans,Thu Jan 30 02:41:11 EST 2014,Louisiana,New Orleans,8,' \
-                                '0,haley,22,60/80/100,5,6,110,DD,30,26,29,caucasian,white,blue,brunette,' \
-                                'rest_type,rest_ethnicity,res_age,9802534087;5182561877,NC;NY,charlotte;' \
-                                'albany,john@smith.com,www.johnsmith.com,johnsmithmedia,' \
-                                'Louisiana_2014_1_30_1391067671000_6_0.jpg;Louisiana_2014_1_30_1391067671000_6_1.jpg;' \
-                                'Louisiana_2014_1_30_1391067671000_6_2.jpg;Louisiana_2014_1_30_1391067671000_6_3.jpg;' \
-                                'Louisiana_2014_1_30_1391067671000_6_4.jpg;Louisiana_2014_1_30_1391067671000_6_5.jpg'
+        self._features_full = '9552601,neworleans,Thu Jan 30 02:41:11 EST 2014,Louisiana,New Orleans,8,' \
+                              '0,haley,22,60/80/100,66,110,DD,30,26,29,caucasian,white,blue,brunette,' \
+                              'rest_type,rest_ethnicity,res_age,9802534087;5182561877,NC;NY,charlotte;' \
+                              'albany,john@smith.com,www.johnsmith.com,johnsmithmedia,' \
+                              'Louisiana_2014_1_30_1391067671000_6_0.jpg;Louisiana_2014_1_30_1391067671000_6_1.jpg;' \
+                              'Louisiana_2014_1_30_1391067671000_6_2.jpg;Louisiana_2014_1_30_1391067671000_6_3.jpg;' \
+                              'Louisiana_2014_1_30_1391067671000_6_4.jpg;Louisiana_2014_1_30_1391067671000_6_5.jpg'
+        self._features_full = self._features_full.split(',')
+        self._feature_types = ('int,string,date,string,string,int,int,string,int,string,int,int,string,int,int,int,'
+                               'string,string,string,string,,,,int,string,string,string,string,string,'
+                               'string').split(',')
+        self._r0 = Record(0, len(self._features_full))
+        self._r1 = Record(0, len(self._features_full))
 
     def test_initialize_from_full_annotation(self):
-        self._r1.initialize_from_annotation(self._annotation_full, 0)
-        self.assertSetEqual(self._r1.ads, {0})
-        self.assertSetEqual(self._r1.posters, {'9552601'})
-        self.assertSetEqual(self._r1.sites, {'neworleans'})
-        self.assertSetEqual(self._r1.dates, {datetime.datetime(2014, 1, 30, 2, 41, 11)})
-        self.assertSetEqual(self._r1.states, {'Louisiana'})
-        self.assertSetEqual(self._r1.cities, {'New Orleans'})
-        self.assertEqual(self._r1.perspective_1st, 0)  # currently holding at zero
-        self.assertEqual(self._r1.perspective_3rd, 0)  # currently holding at zero
-        self.assertSetEqual(self._r1.names, {'haley'})
-        self.assertSetEqual(self._r1.ages, {22})
-        self.assertSetEqual(self._r1.costs, {'60/80/100'})
-        self.assertSetEqual(self._r1.heights, {66})
-        self.assertSetEqual(self._r1.weights, {110})
-        self.assertSetEqual(self._r1.cups, {'DD'})
-        self.assertSetEqual(self._r1.chests, {30})
-        self.assertSetEqual(self._r1.waists, {26})
-        self.assertSetEqual(self._r1.hips, {29})
-        self.assertSetEqual(self._r1.ethnicities, {'caucasian'})
-        self.assertSetEqual(self._r1.skincolors, {'white'})
-        self.assertSetEqual(self._r1.eyecolors, {'blue'})
-        self.assertSetEqual(self._r1.haircolors, {'brunette'})
-        self.assertSetEqual(self._r1.phones, {'9802534087', '5182561877'})
-        self.assertSetEqual(self._r1.emails, {'john@smith.com'})
-        self.assertSetEqual(self._r1.urls, {'www.johnsmith.com'})
-        self.assertSetEqual(self._r1.medias, {'johnsmithmedia'})
-        self.assertSetEqual(self._r1.images, {'Louisiana_2014_1_30_1391067671000_6_0.jpg',
-                                              'Louisiana_2014_1_30_1391067671000_6_1.jpg',
-                                              'Louisiana_2014_1_30_1391067671000_6_2.jpg',
-                                              'Louisiana_2014_1_30_1391067671000_6_3.jpg',
-                                              'Louisiana_2014_1_30_1391067671000_6_4.jpg',
-                                              'Louisiana_2014_1_30_1391067671000_6_5.jpg'})
+        self._r0.initialize_from_annotation(self._features_full, self._feature_types)
+        self.assertSetEqual(self._r0.ads, {0})
+        self.assertSetEqual(self._r0.features[0], {9552601})
+        self.assertSetEqual(self._r0.features[1], {'neworleans'})
+        self.assertSetEqual(self._r0.features[2], {datetime.datetime(2014, 1, 30, 2, 41, 11)})
+        self.assertSetEqual(self._r0.features[3], {'Louisiana'})
+        self.assertSetEqual(self._r0.features[4], {'New Orleans'})
+        self.assertEqual(self._r0.features[5], {8})
+        self.assertEqual(self._r0.features[6], {0})
+        self.assertSetEqual(self._r0.features[7], {'haley'})
+        self.assertSetEqual(self._r0.features[8], {22})
+        self.assertSetEqual(self._r0.features[9], {'60/80/100'})
+        self.assertSetEqual(self._r0.features[10], {66})
+        self.assertSetEqual(self._r0.features[11], {110})
+        self.assertSetEqual(self._r0.features[12], {'DD'})
+        self.assertSetEqual(self._r0.features[13], {30})
+        self.assertSetEqual(self._r0.features[14], {26})
+        self.assertSetEqual(self._r0.features[15], {29})
+        self.assertSetEqual(self._r0.features[16], {'caucasian'})
+        self.assertSetEqual(self._r0.features[17], {'white'})
+        self.assertSetEqual(self._r0.features[18], {'blue'})
+        self.assertSetEqual(self._r0.features[19], {'brunette'})
+        self.assertSetEqual(self._r0.features[23], {9802534087, 5182561877})
+        self.assertSetEqual(self._r0.features[26], {'john@smith.com'})
+        self.assertSetEqual(self._r0.features[27], {'www.johnsmith.com'})
+        self.assertSetEqual(self._r0.features[28], {'johnsmithmedia'})
+        self.assertSetEqual(self._r0.features[29], {'Louisiana_2014_1_30_1391067671000_6_0.jpg',
+                                                    'Louisiana_2014_1_30_1391067671000_6_1.jpg',
+                                                    'Louisiana_2014_1_30_1391067671000_6_2.jpg',
+                                                    'Louisiana_2014_1_30_1391067671000_6_3.jpg',
+                                                    'Louisiana_2014_1_30_1391067671000_6_4.jpg',
+                                                    'Louisiana_2014_1_30_1391067671000_6_5.jpg'})
 
     def test_eq(self):
-        self.assertEqual(self._r1, self._r2)
-        self._r1.initialize_from_annotation(self._annotation_full, 0)
-        self.assertNotEqual(self._r1, self._r2)
-        self._r2.initialize_from_annotation(self._annotation_full, 0)
-        self.assertEqual(self._r1, self._r2)
+        self.assertEqual(self._r0, self._r1)
+        self._r0.initialize_from_annotation(self._features_full, self._feature_types)
+        self.assertNotEqual(self._r0, self._r1)
+        self._r1.initialize_from_annotation(self._features_full, self._feature_types)
+        self.assertEqual(self._r0, self._r1)
 
     def test_initialize_from_empty_annotation(self):
-        annotation_empty = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'  # 30 fields
-        self._r1.initialize_from_annotation(annotation_empty, 0)
-        self._r2.add_ad(0)
-        self.assertEqual(self._r1, self._r2)
-
-    def test_initialize_from_none_annotation(self):
-        annotation_none = 'none,none,none,none,none,none,none,none,none,none,none,none,none,none,none,none,none,none,' \
-                          'none,none,none,none,none,none,none,none,none,none,none,none,none'
-        self._r1.initialize_from_annotation(annotation_none, 0)
-        self._r2.add_ad(0)
-        self.assertEqual(self._r1, self._r2)
+        annotation_empty = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,'.split(',')
+        self._r0.initialize_from_annotation(annotation_empty, self._feature_types)
+        self.assertEqual(self._r0, self._r1)
 
     def test_merge(self):
-        self._r1.merge(self._r2)
-        self.assertEqual(self._r1, self._r2)
-        self._r1.initialize_from_annotation(self._annotation_full, 0)
-        self._r1.merge(self._r2)
-        self.assertNotEqual(self._r1, self._r2)
-        self._r2.merge(self._r1)
-        self.assertEqual(self._r1, self._r2)
+        self._r0.merge(self._r1)
+        self.assertEqual(self._r0, self._r1)
+        self._r0.initialize_from_annotation(self._features_full, self._feature_types)
+        self._r0.merge(self._r1)
+        self.assertNotEqual(self._r0, self._r1)
+        self._r1.merge(self._r0)
+        self.assertEqual(self._r0, self._r1)
 
     def test_month(self):
         m = month('Apr')
