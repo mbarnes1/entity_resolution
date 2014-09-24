efcc8607a0cca8a89bff68d1991f3dd11e82e2fa
Matt Barnes <barnes.matt.j@gmail.com>
2014-09-24 11:19:51 -0400

    Generalized and tested database class.

--- database.py
+++ database.py
@@ -1,35 +1,63 @@
-#/usr/bin/python
-
-import cPickle as pickle  # for saving shit
 from record import Record
 import numpy as np
+__author__ = 'mbarnes1'
 
 
-# .csv records are of the format posterid,site,Date,State,City,Perspective_1st,Perspective_3rd,
-# Name,Age,Cost,Height_ft,Height_in,Weight,Cup,Chest,Waist,Hip,Ethnicity,SkinColor,EyeColor,
-# HairColor,Restriction_Type,Restriction_Ethnicity,Restriction_Age,PhoneNumber,AreaCode_State,
-# AreaCode_Cities,Email,Url,Media,Images
 class RecordDatabase(object):
+    """
+    A collection of record objects, using a dictionary with [index, record object]
+    Reads from a flat file of the format:
+    FeatureName1, FeatureName2, ..., FeatureNameN
+    FeatureType1, FeatureType2, ..., FeatureTypeN
+    FeatureStrength1, FeatureStrength2, ..., FeatureStrengthN
+    Blocking1, Blocking2, ..., BlockingN
+    PairwiseUse1, PairwiseUse2, ..., PairwiseUseN
+    Ad1_Feature1, Ad1_Feature2, ..., Ad1_FeatureN
+    .................
+    AdM_Feature1, AdM_Feature2, ..., AdM_FeatureN
+    
+    where M is the number of ads and N is the number of features
+    """
     def __init__(self, annotation_path, max_records=np.Inf):
         self._annotation_path = annotation_path
         self.records = dict()
         self._max_records = max_records
-        #print 'Opening file...'
         ins = open(self._annotation_path, 'r')
-        ins.next()
-        linecounter = 0  # line counter. Zero indexed
+        feature_names = next(ins).split(',')  # skip the first line, its a header
+        feature_types = next(ins).split(',')  # variable type (e.g. int, string, date)
+        ignore_indices = find_empty_entries(feature_types)
+        self.feature_names = remove_indices(ignore_indices, feature_names)
+        self.feature_types = remove_indices(ignore_indices, feature_types)
+        self.feature_strengths = remove_indices(ignore_indices, next(ins).split(','))
+        self.pairwise_uses = remove_indices(ignore_indices, next(ins).split(','))
+        self.blocking = remove_indices(ignore_indices, next(ins).split(','))
 
         # Loop through all the records
-        for sample in ins:
-            print 'Extracting sample', linecounter
-            r = Record()  # record object from utils
-            r.initialize_from_annotation(sample.rstrip('\n'), linecounter)
-            self.records[linecounter] = r
-            linecounter += 1  # increment node index counter
-            if linecounter >= self._max_records:
+        for ad_number, sample in enumerate(ins):
+            print 'Extracting sample', ad_number
+            r = Record(ad_number, len(self.feature_names))  # record object from utils
+            features = remove_indices(ignore_indices, sample.rstrip('\n').split(','))
+            r.initialize_from_annotation(features, self.feature_types)
+            self.records[ad_number] = r
+            if ad_number >= self._max_records-1:
                 break
-        #print 'Finished generating database'
 
-    def dump(self, dump_path):
-        print 'Pickling record hash table...'
-        pickle.dump(self, open(dump_path, 'wb'))
+
+def remove_indices(remove, lst):
+    """
+    Removes indices from a list
+    :param remove: A list of indices to remove from lst
+    :param lst: List to remove entries from
+    :return new_lst: A new list with entries from lst removed
+    """
+    new_lst = [item for i, item in enumerate(lst) if i not in remove]
+    return new_lst
+
+
+def find_empty_entries(lst):
+    """
+    Finds empty list entries and returns their indicies
+    :param lst: List to find empty entries
+    :return i: Indices of empty entries in lst
+    """
+    return [i for i, x in enumerate(lst) if x == '']
--- test/test_database.py
+++ test/test_database.py
@@ -2,21 +2,31 @@ import unittest
 import sys
 
 sys.path.append('../../')
-from entity_resolution.database import RecordDatabase
+from entity_resolution.database import RecordDatabase, remove_indices, find_empty_entries
 
 __author__ = 'mbarnes1'
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self._test_path = 'test_annotations.txt'
-        self._database = RecordDatabase(self._test_path)
+        self._test_path = 'test_annotations.csv'
+
+    def test_remove_indices(self):
+        lst = ['one', '', 'two', '', 'three', '']
+        new_list = remove_indices([1, 3, 5], lst)
+        self.assertEqual(new_list, ['one', 'two', 'three'])
+
+    def test_find_empty_entries(self):
+        lst = ['one', '', 'two', '', 'three', '']
+        empty_entries = find_empty_entries(lst)
+        self.assertEqual([1, 3, 5], empty_entries)
 
     def test_size(self):
+        self._database = RecordDatabase(self._test_path)
         self.assertEqual(len(self._database.records), 4)
 
     def test_max_size(self):
-        database = RecordDatabase('test_annotations_10000.txt', 409)
+        database = RecordDatabase('test_annotations_10000.csv', 409)
         self.assertEqual(len(database.records), 409)
 
 if __name__ == '__main__':
