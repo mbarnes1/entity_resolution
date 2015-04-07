66cf900ce20c344ba7e5e0b9521edcefce623a8a
Matt Barnes <barnes.matt.j@gmail.com>
2015-04-07 15:14:01 -0400

    Capability to sample multiple databases at once (with none of the same records)

--- python/utils/sample_clusters.py
+++ python/utils/sample_clusters.py
@@ -10,16 +10,18 @@ import numpy as np
 
 
 def main():
-    number_samples = 1000
-    cluster_path = '../test/test_annotations_10000_cleaned_strong_clusters.csv'  # '/home/scratch/trafficjam/entity_resolution_outputs/strong_clusters', 'w')
+    number_samples = [500, 1000]
+    number_databases = 3
+    cluster_path = '../test/test_annotations_10000_cleaned_strong_clusters.csv'  # '/home/scratch/trafficjam/entity_resolution_outputs/strong_clusters.csv'
     annotations_path = '../test/test_annotations_10000_cleaned.csv'  # '/home/scratch/trafficjam/deduped/US_Canada_Extraction_dedup.csv'
-    header_path = '../test/test_annotations_10000_cleaned_header.csv'  # 'US_Canada_Extraction_dedup_header.csv'
-    out_path = 'cluster_subsample_'+str(number_samples)+'.csv'  # '/home/scratch/trafficjam/entity_resolution_outputs/cluster_subsample_'
+    header_path = '../test/test_annotations_10000_cleaned_header.csv'  # '/home/scratch/trafficjam/entity_resolution_outputs/US_Canada_Extraction_dedup_header.csv'
+
     database = Database(annotations_path, header_path=header_path)
     ins = open(cluster_path, 'r')
     cluster_to_indices = dict()
     next(ins)  # skip header
-    for line in ins:
+    for counter, line in enumerate(ins):
+        print 'Loading cluster file', counter
         line = line.rstrip('\n').split(',')
         index = int(line[0])
         cluster_id = int(line[2])
@@ -30,17 +32,21 @@ def main():
     cluster_probabilities = list()
     for cluster, indices in cluster_to_indices.iteritems():
         cluster_probabilities.append(float(len(indices))/len(database.records))
-    cluster_samples = np.random.choice(cluster_to_indices.keys(), number_samples, p=cluster_probabilities)
-    new_database = Database()
-    new_database.feature_descriptor = database.feature_descriptor
+    print 'Sampling clusters'
+    cluster_samples = np.random.choice(cluster_to_indices.keys(), sum(number_samples)*number_databases, p=cluster_probabilities)
     counter = 0
-    while len(new_database.records) < number_samples:
-        cluster = cluster_samples[counter]
-        counter += 1
-        indices = cluster_to_indices[cluster]
-        for index in indices:
-            new_database.records[index] = database.records[index]
-    new_database.dump(out_path)
+    for n in number_samples:
+        for j in range(0, number_databases):
+            out_path = 'cluster_subsample'+str(j)+'_'+str(n)+'.csv'  # '/home/scratch/trafficjam/entity_resolution_outputs/cluster_subsample.csv'
+            new_database = Database()
+            new_database.feature_descriptor = database.feature_descriptor
+            while len(new_database.records) < n:
+                cluster = cluster_samples[counter]
+                counter += 1
+                indices = cluster_to_indices[cluster]
+                for index in indices:
+                    new_database.records[index] = database.records[index]
+            new_database.dump(out_path)
 
 
 if __name__ == '__main__':
