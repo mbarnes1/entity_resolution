c10daf769c99512dfc8350b4b175e2a33b039946
Matt Barnes <barnes.matt.j@gmail.com>
2015-09-20 20:35:07 -0400

    Subsample multiple datasets at the same time (so no overlap)

--- python/utils/sample_clusters.py
+++ python/utils/sample_clusters.py
@@ -9,11 +9,12 @@ from random import shuffle
 
 def main():
     # User parameters. Input labels are a csv of form annotation_index (0 indexed), text_index (1 indexed), entity_id
-    number_samples = 10000
+    number_samples = [10000, 100000, 1000000]
+    number_databases = 2
     truth_path = '../../../entity_resolution_inputs/rebuild_phone_clusters.csv'
-    out_path = '../../../entity_resolution_inputs/rebuild_clusters_10000.csv'
-    max_records_per_entity = 1000  # do not sample extremely large clusters (a hack to prevent entire subsample being a single large entity)
-    max_entity_percentage = 0.3  # do not sample clusters which would constitute larger than this percentage of requested number of samples
+    out_path_prefix = '../../../entity_resolution_inputs/rebuild_clusters'
+    max_records_per_entity = 10000  # do not sample extremely large clusters (a hack to prevent entire subsample being a single large entity)
+    max_entity_percentage = 0.2  # do not sample clusters which would constitute larger than this percentage of requested number of samples
 
     # Load the data
     ins = open(truth_path, 'r')
@@ -45,21 +46,26 @@ def main():
 
     # Output the results
     counter = 0
-    sampled_records = []
-    while len(sampled_records) < number_samples:
-        entity = entity_samples[counter]
-        counter += 1
-        records = entity_to_records[entity]
-        if len(records) < min(max_records_per_entity, max_entity_percentage*number_samples):  # only clusters less than 10000 and less than 30%
-            sampled_records.extend(records)
-    print 'Shuffling results'
-    shuffle(sampled_records)
-    print 'Writing results'
-    ins = open(out_path, 'w')
-    ins.write('annotations_line (0 indexed), text_line (1 indexed), cluster_id\n')
-    for sampled_record in sampled_records[:number_samples]:
-        ins.write(str(text_to_annotation[sampled_record]) + ',' + str(sampled_record) + ',' + str(text_to_entity[sampled_record]) + '\n')
-    ins.close()
+    for n in number_samples:
+        for j in range(0, number_databases):
+            out_path = out_path_prefix + str(j) + '_'+str(n)+'.csv'
+            ins = open(out_path, 'w')
+            ins.write('annotations_line (0 indexed), text_line (1 indexed), cluster_id\n')
+            sampled_records = []
+            while len(sampled_records) < n:
+                entity = entity_samples[counter]
+                counter += 1
+                records = entity_to_records[entity]
+                if len(records) < min(max_records_per_entity, max_entity_percentage*number_samples):
+                    sampled_records.extend(records)
+            print 'Shuffling results'
+            shuffle(sampled_records)
+            print 'Writing results'
+            ins = open(out_path, 'w')
+            ins.write('annotations_line (0 indexed), text_line (1 indexed), cluster_id\n')
+            for sampled_record in sampled_records[:number_samples]:
+                ins.write(str(text_to_annotation[sampled_record]) + ',' + str(sampled_record) + ',' + str(text_to_entity[sampled_record]) + '\n')
+            ins.close()
 
 
 if __name__ == '__main__':
